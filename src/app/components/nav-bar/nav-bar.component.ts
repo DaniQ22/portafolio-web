866db0c6c071b2e0cbf2b734df551e0f2ff7e2a8
Daniel Quiñonez <daniel.jose1999@outlook.com>
2025-02-10 12:46:41 -0500

    redirecciones nav

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, NgClass } from '@angular/common';
+import { CommonModule, NgClass, ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,12 +10,22 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  constructor(private viewportScroller: ViewportScroller) { }
+
+
+
   isActiveMenu: boolean = false;
 
   toggleMenu() {
     this.isActiveMenu = !this.isActiveMenu;
     console.log('Menu state:', this.isActiveMenu); // Para debugging
 
+  }
+
+
+
+  scrollToElement(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
   }
 
 }
