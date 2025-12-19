fa4f8b535b5f1c63757c322cc9b997c8f4df7eba
Daniel Quiñonez <daniel.jose1999@outlook.com>
2025-12-19 14:40:03 -0500

    new version

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Project {
+  title: string;
+  image?: string;       // imagen principal (opcional)
+  icons: string[];      // iconos de tecnologías
+  url?: string;
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+
+
+  constructor(private readonly router: Router) { }
+
+
+  projects: Project[] = [
+    {
+      title: 'Mi Portafolio',
+      image: 'assets/images/portafolio.png',
+      icons: ['assets/icons/angular.svg'],
+    },
+    {
+      title: 'Nube de imágenes',
+      icons: ['assets/icons/angular.svg', 'assets/icons/spring.svg', 'assets/icons/postgresql.svg']
+    },
+    {
+      title: 'Nube de imágenes',
+      image: 'assets/images/plan-project.png',
+      icons: ['assets/icons/angular.svg', 'assets/icons/nestjs.svg'],
+      url: 'https://plans-front.vercel.app'
+    }
+  ];
+
+
+  openProject(url: any) {
+    console.log('open project to ' + url);
+    if (!url || url === undefined || url === '') return;
+
+    // Abre en una nueva pestaña
+    window.open(url, '_blank');
+  }
 
 }
